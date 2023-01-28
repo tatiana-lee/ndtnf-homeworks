6cf17d2fc6f51d7790866e5c554bd18b2fed3d49
satania-lee <rockky.tl@gmail.com>
2023-01-28 15:26:19 +0300

    rxjs task

--- 009-rxjs/src/index.ts
+++ 009-rxjs/src/index.ts
@@ -1,9 +1,35 @@
-import {of, from, timer, range} from 'rxjs';
+import { of, fromEvent } from 'rxjs';
+import { switchMap, map, debounceTime, distinctUntilChanged } from 'rxjs/operators';
+import { ajax } from 'rxjs/ajax';
 
-const o = range(0, 10)
+const search = document.getElementById('search')
+const url = 'https://api.github.com/search/repositories?q='
+const gitLabUrl = 'https://gitlab.com/api/v4/projects?search='
 
-o.subscribe({
-  next: (value: any) => console.log('Next:', value),
+const search$ = fromEvent(search, 'input')
+  .pipe(
+    map(e => (e.target as HTMLInputElement).value),
+    debounceTime(1000),
+    distinctUntilChanged(),
+    switchMap(v => ajax.getJSON(url + v))
+  )
+
+search$.subscribe({
+  next: (value: any) => console.log('Repositories:', value.total_count),
   complete: () => console.log('Complete!'),
   error: (error) => console.log('Error!', error)
+})
+
+const gitLab$ = fromEvent(search, 'input')
+.pipe(
+  map(e => (e.target as HTMLInputElement).value),
+  debounceTime(1000),
+  distinctUntilChanged(),
+  switchMap(v => ajax.getJSON(gitLabUrl + v))
+)
+
+gitLab$.subscribe({
+next: (value: any) => console.log('Projects:', value),
+complete: () => console.log('Complete!'),
+error: (error) => console.log('Error!', error)
 })
