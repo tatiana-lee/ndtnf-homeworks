8d9f7ccf36d8ca412aa3d766c09705e5ee72d0aa
satania-lee <rockky.tl@gmail.com>
2023-02-07 13:52:21 +0300

    change secretOrKey to env file

--- 006-nestjs-ext/nest-library/src/auth/auth.module.ts
+++ 006-nestjs-ext/nest-library/src/auth/auth.module.ts
@@ -4,13 +4,18 @@ import { JwtStrategy } from './jwt.strategy';
 import { UsersModule } from '../users/users.module';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
+import config from './config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      load: [config],
+    }),
     UsersModule,
     PassportModule,
     JwtModule.register({
-      secret: 'secret',
+      secret: process.env.JWT_TOKEN,
       signOptions: { expiresIn: '120s' },
     }),
   ],
--- 006-nestjs-ext/nest-library/src/auth/jwt.strategy.ts
+++ 006-nestjs-ext/nest-library/src/auth/jwt.strategy.ts
@@ -2,13 +2,17 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthService } from './auth.service';
+// import { key } from './config';
+
+// console.log(key.secret)
+// console.log(process.env.JWT_TOKEN)
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(private authService: AuthService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: 'secret',
+      secretOrKey: process.env.JWT_TOKEN,
     });
   }
 
